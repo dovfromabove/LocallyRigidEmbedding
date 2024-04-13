a0cad0b0aad8387524979ec1d583ca4e8418fe5f
AntonTchaplianka <anton.ch@gmail.com>
2024-04-13 15:09:20 +0300

    Updating Results

--- results.py
+++ results.py
@@ -1,5 +1,6 @@
 import os
 import numpy as np
+from sklearn.metrics import euclidean_distances
 
 from matplotlib import pyplot as plt
 from matplotlib import colors
@@ -20,8 +21,11 @@ def plot_numerical_spectrum(eigenvalues, plot_coord, axs, cutoff=10, baseline=0,
         addlabels(list(range(len(normalized_values))), normalized_values, axs[plot_coord])
     axs[plot_coord].set_xticks(np.arange(len(normalized_values)))
 
-def plot_numerical_spectrums(evl_no_noise, evl_1p_noise, evl_10p_noise, filename, labels=False):
-    _, axs = plt.subplots(1, 3, figsize=(25,3))
+
+def plot_numerical_spectrums(evl_no_noise, evl_1p_noise, evl_10p_noise, filename, labels=False, k=None):
+    fig, axs = plt.subplots(1, 3, figsize=(25,3))
+    if k:
+        fig.text(0.07, 0.48, rf'$k={k}$', fontsize=20, ha='center')
     plot_numerical_spectrum(evl_no_noise, 0, axs, labels=labels)
     plot_numerical_spectrum(evl_1p_noise, 1, axs, labels=labels)
     plot_numerical_spectrum(evl_10p_noise, 2, axs, labels=labels)
@@ -53,39 +57,62 @@ def homogenous_transformation(array, degrees, translations):
     modified_array = np.hstack([array, np.zeros((array.shape[0], 1)) ,np.ones((array.shape[0], 1))])
     return (modified_array @ transformation_matrix.T)[:,0:-2]
 
-def plot_embedding(R, subplot_coords, axs, title, x_coords=r"$\phi_1$", y_coords=r"$\phi_2$", plot_coordinates=[0,1], ref_coords=None, margin=0.05, grid=False):
+def embedding_error(ground_truth, embedding):
+    return np.square(np.linalg.norm(euclidean_distances(ground_truth)  - euclidean_distances(embedding)))
+
+def plot_embedding(R, subplot_coords, axs, title, x_coords=r"$\phi_1$", y_coords=r"$\phi_2$", plot_coordinates=[0,1], ref_coords=None, margin=0.05, grid=False, set_title=True):
     if ref_coords is None:
         ref_coords = R
     i = subplot_coords
     # i,j = subplot_coords
     norm = colors.Normalize(vmin=min(ref_coords[:, 0]), vmax=max(ref_coords[:, 0]))
     axs[i].scatter(R[:, plot_coordinates[0]], R[:, plot_coordinates[1]], c=ref_coords[:, 0], s=5, cmap=plt.cm.rainbow, norm=norm)
-    axs[i].set_title(title)
+    if set_title:
+        axs[i].set_title(title)
     axs[i].set(xlabel=x_coords, ylabel=y_coords)
     xmin, xmax, ymin, ymax = min(ref_coords[:, 0]), max(ref_coords[:, 0]), min(ref_coords[:, 1]), max(ref_coords[:, 1])
     axs[i].set_xlim([xmin-margin, xmax+margin])
     axs[i].set_ylim([ymin-margin, ymax+margin])
     if grid:
         axs[i].grid(visible='both', alpha=0.2)
+    axs[i].spines['top'].set_visible(False)
+    axs[i].spines['right'].set_visible(False)
+    axs[i].spines['bottom'].set_visible(False)
+    axs[i].spines['left'].set_visible(False)
 
-def plot_embeddings(us_cities, r_no_noise, r_1p_noise, r_10p_noise, filename, margin=0.05, grid=False):
-    _, axs = plt.subplots(1, 4, figsize=(25,4))
-    plot_embedding(us_cities, 0, axs, title='US Cities', x_coords=r'$x$', y_coords=r'$y$', margin=margin, grid=grid)
-    plot_embedding(r_no_noise, 1, axs, title='Locally Rigid Embedding: no noise', x_coords=r'$\phi_1$', y_coords=r'$\phi_2$', ref_coords=us_cities, margin=margin, grid=grid)
-    plot_embedding(r_1p_noise, 2, axs, title='Locally Rigid Embedding: 1% noise', x_coords=r'$\phi_1$', y_coords=r'$\phi_2$', ref_coords=us_cities, margin=margin, grid=grid)
-    plot_embedding(r_10p_noise, 3, axs, title='Locally Rigid Embedding: 10% noise', x_coords=r'$\phi_1$', y_coords=r'$\phi_2$', ref_coords=us_cities, margin=margin, grid=grid)
+
+def plot_embeddings(us_cities, r_no_noise, r_1p_noise, r_10p_noise, filename, margin=0.05, grid=False, k=None, plot_cities=True, set_title=True):
+    if plot_cities:
+        fig, axs = plt.subplots(1, 4, figsize=(25,4))
+        i=1
+        plot_embedding(us_cities, 0, axs, title='US Cities', x_coords=r'$x$', y_coords=r'$y$', margin=margin, grid=grid, set_title=set_title)
+    else:
+        fig, axs = plt.subplots(1, 3, figsize=(22,4))
+        i=0
+    if k:
+        fig.text(0.06, 0.48, rf'$k={k}$', fontsize=20, ha='center')
+    plot_embedding(r_no_noise, i, axs, title='Locally Rigid Embedding: no noise', x_coords=r'$\phi_1$', y_coords=r'$\phi_2$', ref_coords=us_cities, margin=margin, grid=grid, set_title=set_title)
+    plot_embedding(r_1p_noise, i+1, axs, title='Locally Rigid Embedding: 1% noise', x_coords=r'$\phi_1$', y_coords=r'$\phi_2$', ref_coords=us_cities, margin=margin, grid=grid, set_title=set_title)
+    plot_embedding(r_10p_noise, i+2, axs, title='Locally Rigid Embedding: 10% noise', x_coords=r'$\phi_1$', y_coords=r'$\phi_2$', ref_coords=us_cities, margin=margin, grid=grid, set_title=set_title)
     plt.savefig(filename)
     print(f'Saved image of LRE results: {filename}')
 
-
-def show_results(k, directory):
-    directory = f'output_070424_k{k}'
+def print_numerical_results(directory):
     us_cities = load_matrix(os.path.join(directory, 'us_cities.npy'))
 
-    spectrum_no_noise = load_matrix(os.path.join(directory,'evl_no_noise.npy'))
-    spectrum_1p_noise = load_matrix(os.path.join(directory,'evl_1%_noise.npy'))
-    spectrum_10p_noise = load_matrix(os.path.join(directory,'evl_10%_noise.npy'))
-    plot_numerical_spectrums(spectrum_no_noise, spectrum_1p_noise, spectrum_10p_noise, os.path.join(directory,f'eval_spectrum_k{k}.png'), labels=False)
+    r_no_noise = load_matrix(os.path.join(directory,'r_no_noise.npy'))
+    r_1p_noise = load_matrix(os.path.join(directory,'r_1%_noise.npy'))
+    r_10p_noise = load_matrix(os.path.join(directory,'r_10%_noise.npy'))
+    print(f"No noise embedding error ", embedding_error(us_cities, r_no_noise))
+    print(f"1% noise embedding error ", embedding_error(us_cities, r_1p_noise))
+    print(f"10% noise embedding error ", embedding_error(us_cities, r_10p_noise))
+
+    eigenvalues_no_noise = load_matrix(os.path.join(directory,'evl_no_noise.npy'))
+    eigenvalues_1p_noise = load_matrix(os.path.join(directory,'evl_1%_noise.npy'))
+    eigenvalues_10p_noise = load_matrix(os.path.join(directory,'evl_10%_noise.npy'))
+    print('No noise eigenvalues: ', np.sort(np.abs(eigenvalues_no_noise))[0:3])
+    print('1% noise eigenvalues: ', np.sort(np.abs(eigenvalues_1p_noise))[0:4])
+    print('10% noise eigenvalues: ', np.sort(np.abs(eigenvalues_10p_noise))[0:5])
 
     singular_values_no_noise = load_matrix(os.path.join(directory,'singular_val_no_noise.npy'))
     singular_values_1p_noise = load_matrix(os.path.join(directory,'singular_val_1%_noise.npy'))
@@ -94,13 +121,25 @@ def show_results(k, directory):
     print('1% noise singular values: ', singular_values_1p_noise)
     print('10% noise singular values: ', singular_values_10p_noise)
 
+
+def show_results(k, directory):
+    us_cities = load_matrix(os.path.join(directory, 'us_cities.npy'))
+
+    spectrum_no_noise = load_matrix(os.path.join(directory,'evl_no_noise.npy'))
+    spectrum_1p_noise = load_matrix(os.path.join(directory,'evl_1%_noise.npy'))
+    spectrum_10p_noise = load_matrix(os.path.join(directory,'evl_10%_noise.npy'))
+    plot_numerical_spectrums(spectrum_no_noise, spectrum_1p_noise, spectrum_10p_noise, os.path.join(directory,f'eval_spectrum_k{k}.png'), labels=False, k=k)
+
     r_no_noise = load_matrix(os.path.join(directory,'r_no_noise.npy'))
     r_1p_noise = load_matrix(os.path.join(directory,'r_1%_noise.npy'))
     r_10p_noise = load_matrix(os.path.join(directory,'r_10%_noise.npy'))
-    
-    r_no_noise = homogenous_transformation(r_no_noise, [0,0,0], [0.00,0.00,0])
-    r_1p_noise = homogenous_transformation(r_1p_noise, [0,0,0], [0.00,0.00,0])
-    r_10p_noise = homogenous_transformation(r_10p_noise, [0,0,0], [0.00,0.00,0])
-    plot_embeddings(us_cities, r_no_noise, r_1p_noise, r_10p_noise, os.path.join(directory, f'LRE_k{k}.png'), grid=True)
+    r_no_noise = homogenous_transformation(r_no_noise, [0,0,-17.5], [0.04,0.020,0])
+    r_1p_noise = homogenous_transformation(r_1p_noise, [180,0,7], [0.04,0.03,0])
+    r_10p_noise = homogenous_transformation(r_10p_noise, [0,0,-2], [0.04,0.05,0])
+    plot_embeddings(us_cities, r_no_noise, r_1p_noise, r_10p_noise, os.path.join(directory, f'LRE_k{k}.png'), grid=True, k=k, plot_cities=True, set_title=True)
 
-    
+if __name__ == '__main__':
+    k = 8
+    dir = f'output_070424_k{k}'
+    show_results(k, dir)
+    print_numerical_results(dir)
